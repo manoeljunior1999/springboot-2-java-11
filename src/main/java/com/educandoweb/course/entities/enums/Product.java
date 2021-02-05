b781a1684d4ce020a1ff9af858af3d49c6f449a1
Manoel Luis <manoelluisjunior@hotmail.com>
2021-02-05 15:05:06 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/enums/Product.java
+++ src/main/java/com/educandoweb/course/entities/enums/Product.java
@@ -1,4 +1,4 @@
-package com.educandoweb.course.entities;
+package com.educandoweb.course.entities.enums;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -11,28 +11,35 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import com.educandoweb.course.entities.enums.Product;
+import com.educandoweb.course.entities.Category;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {	
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
-	@Transient
-	private Set<Product> products = new HashSet<>();
 	
-	public Category() {
+@Transient
+	private Set<Category> categories = new HashSet<>();
+	
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -50,8 +57,33 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -70,12 +102,13 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
+	}	
+	
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,12 +2,11 @@ package com.educandoweb.course.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+
 
 @Service
 public class UserService {
